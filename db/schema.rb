df63a1e8561cc787e0b56efb9998157e90cfb681
Im Genius <im.genius.man@gmail.com>
2018-08-17 14:37:25 +0500

    Change db from sqlite3 to posgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_02_103617) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bugs", force: :cascade do |t|
     t.string "title"
     t.date "deadline"
@@ -40,8 +43,8 @@ ActiveRecord::Schema.define(version: 2018_07_02_103617) do
   end
 
   create_table "user_projects", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "project_id"
+    t.bigint "user_id"
+    t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_user_projects_on_project_id"
@@ -67,4 +70,6 @@ ActiveRecord::Schema.define(version: 2018_07_02_103617) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_projects", "projects"
+  add_foreign_key "user_projects", "users"
 end
